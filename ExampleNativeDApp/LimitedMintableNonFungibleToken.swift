d1a35c573aba52401c91583828460070fddbe565
Josh Pyles <joshpyles@gmail.com>
2018-06-14 16:47:06 -0700

    Update to latest Bitski and Web3

--- ExampleNativeDApp/LimitedMintableNonFungibleToken.swift
+++ ExampleNativeDApp/LimitedMintableNonFungibleToken.swift
@@ -12,25 +12,68 @@ import BigInt
 import Bitski
 
 enum ContractError: Error {
+    case contractNotDeployed
     case unknownBalance
 }
 
-class LimitedMintableNonFungibleToken: EthereumContract {
-    static let contractAddress = try! EthereumAddress(hex: "0x8c51dff8fcd48c292354ee751cceabeb25357df4", eip55: false)
-    let web3: Web3
+class LimitedMintableNonFungibleToken: GenericERC721Contract {
     
-    init(web3: Web3) {
-        self.web3 = web3
+    static let contractAddress = try! EthereumAddress(hex: "0x8c51dff8fcd48c292354ee751cceabeb25357df4", eip55: false)
+    
+    static let Mint = SolidityEvent(name: "Mint", anonymous: false, inputs: [
+        SolidityEvent.Parameter(name: "_to", type: .address, indexed: true),
+        SolidityEvent.Parameter(name: "_tokenId", type: .uint, indexed: true),
+    ])
+    
+    override var events: [SolidityEvent] {
+        return [
+            LimitedMintableNonFungibleToken.Transfer,
+            LimitedMintableNonFungibleToken.Approval,
+            LimitedMintableNonFungibleToken.Mint
+        ]
     }
     
-    //MARK: - Calls
-
+    init(web3: Web3) {
+        super.init(name: "LimitedMintableNonFungibleToken", address: LimitedMintableNonFungibleToken.contractAddress, eth: web3.eth)
+    }
+    
+    required init(name: String, address: EthereumAddress?, eth: Web3.Eth) {
+        super.init(name: name, address: address, eth: eth)
+    }
+    
+    //MARK: - Methods
+    
+    func mint(tokenId: BigUInt) -> SolidityInvocation {
+        let inputs = [SolidityFunctionParameter(name: "_tokenId", type: .uint256)]
+        let function = SolidityNonPayableFunction(name: "mint", inputs: inputs, handler: self)
+        return function.invoke(tokenId)
+    }
+    
+    func getOwnerTokens(owner: EthereumAddress) -> SolidityInvocation {
+        let inputs = [SolidityFunctionParameter(name: "_owner", type: .address)]
+        let outputs = [SolidityFunctionParameter(name: "_tokenIds", type: .array(type: .uint256, length: nil))]
+        let function = SolidityConstantFunction(name: "getOwnerTokens", inputs: inputs, outputs: outputs, handler: self)
+        return function.invoke(owner)
+    }
+    
+    func token(of owner: EthereumAddress, by index: BigUInt) -> SolidityInvocation {
+        let inputs = [
+            SolidityFunctionParameter(name: "_owner", type: .address),
+            SolidityFunctionParameter(name: "_index", type: .uint)
+        ]
+        let outputs = [SolidityFunctionParameter(name: "_tokenId", type: .uint)]
+        let function = SolidityConstantFunction(name: "tokenOfOwnerByIndex", inputs: inputs, outputs: outputs, handler: self)
+        return function.invoke(owner, index)
+    }
+    
+    //MARK: - Convenience
+    
     func getBalance(address: EthereumAddress) -> Promise<EthereumQuantity> {
         return firstly {
-            self.call(functionName: "getBalance(address)", parameters: [address])
-        }.then { balanceData -> Promise<EthereumQuantity> in
-            if let balance = balanceData.ethereumValue().ethereumQuantity {
-                return Promise.value(balance)
+            return self.balanceOf(address: address).call()
+        }.then { values -> Promise<EthereumQuantity> in
+            if let balance = values["_balance"] as? BigUInt {
+                return Promise.value(EthereumQuantity(quantity: balance))
             }
             return Promise(error: ContractError.unknownBalance)
         }
@@ -38,10 +81,12 @@ class LimitedMintableNonFungibleToken: EthereumContract {
     
     func getOwnerTokens(address: EthereumAddress) -> Promise<[BigUInt]> {
         return firstly {
-            self.call(functionName: "getOwnerTokens(address)", parameters: [address])
-        }.then { callData -> Promise<[BigUInt]> in
-            print(callData, callData.hex())
-            return Parser.decodeArray(hexString: callData.hex())
+            return self.getOwnerTokens(owner: address).call()
+        }.then { values -> Promise<[BigUInt]> in
+            if let tokens = values["_tokenIds"] as? [BigUInt] {
+                return Promise.value(tokens)
+            }
+            return Promise(error: ContractError.unknownBalance)
         }
     }
     
@@ -60,21 +105,21 @@ class LimitedMintableNonFungibleToken: EthereumContract {
     
     func token(of owner: EthereumAddress, by index: EthereumValue) -> Promise<EthereumValue> {
         return firstly {
-            self.call(functionName: "tokenOfOwnerByIndex(address,uint256)", parameters: [owner, index])
-        }.then { tokenData -> Promise<EthereumValue> in
-            return Promise.value(tokenData.ethereumValue())
+            token(of: owner, by: try! BigUInt(ethereumValue: index)).call()
+        }.then { values -> Promise<EthereumValue> in
+            if let tokenId = values["_tokenId"] as? BigUInt {
+                return Promise.value(tokenId.ethereumValue())
+            }
+            return Promise(error: ContractError.unknownBalance)
         }
     }
     
-    //MARK: - Sends
-    
-    func mintNewToken(address: EthereumAddress) -> Promise<(BigUInt, EthereumData)> {
-        let functionName = "mint(uint256)"
+    func mintNewToken(from: EthereumAddress) -> Promise<(BigUInt, EthereumData)> {
         do {
-            let tokenID = try Web3.utils.randomHex(bytesCount: 32)
-            let parameters = ["0x\(tokenID)"]
-            return self.send(functionName: functionName, parameters: parameters, fromAddress: address).map { hash in
-                return (BigUInt(tokenID, radix: 16)!, hash)
+            let tokenHex = try Web3.utils.randomHex(bytesCount: 32)
+            let tokenID = BigUInt(hexString: tokenHex)!
+            return mint(tokenId: tokenID).send(from: from, value: nil, gas: 700000, gasPrice: nil).map { hash in
+                return (tokenID, hash)
             }
         } catch {
             return Promise(error: error)
@@ -82,9 +127,10 @@ class LimitedMintableNonFungibleToken: EthereumContract {
     }
     
     func deleteToken(from: EthereumAddress, tokenID: BigUInt) -> Promise<EthereumData> {
-        let functionName = "transfer(address,uint256)"
-        let parameters: [EthereumValueConvertible] = [LimitedMintableNonFungibleToken.contractAddress, tokenID]
-        return self.send(functionName: functionName, parameters: parameters, fromAddress: from)
+        guard let to = address else {
+            return Promise(error: ContractError.contractNotDeployed)
+        }
+        return transfer(to: to, tokenId: tokenID).send(from: from, value: nil, gas: 700000, gasPrice: nil)
     }
     
 }
